a2612a9140d4244f94d0a307554597719e97a6cf
engler <ddd.rrr.eee@gmail.com>
2025-04-03 20:04:39 -0700

    minor

--- labs/2-dynamic-code-gen/prelab-code-pi/3-large-constant.c
+++ labs/2-dynamic-code-gen/prelab-code-pi/3-large-constant.c
@@ -1,9 +1,22 @@
 #include "rpi.h"
 
 // example of how arm loads large values.
+//  - sticks the constant at the end of the code
+//  - does an ldr off the pc.
+//
+// so the C code becomes:
+//   e59f0000    ldr r0, [pc] 
+//   e12fff1e    bx  lr
+//   12345678    .word   0x12345678
 uint32_t ret_large(void) {
     return 0x12345678;
 }
+
+
+
+
+
+
 
 void notmain(void) {
 }
