66662fb1cd5258161a03dbdfa72a8888a06a6276
engler <ddd.rrr.eee@gmail.com>
2025-05-27 15:00:42 -0700

    new

---
+++ labs/16-gpu/code/2-mandelbrot/fat32/code/objs/external-code/unicode-utf8.d
@@ -0,0 +1,19 @@
+objs/external-code/unicode-utf8.o: external-code/unicode-utf8.c \
+ /home/engler/class/cs140e-25win//libpi/include/rpi.h \
+ /home/engler/class/cs140e-25win//libpi/libc/demand.h \
+ /home/engler/class/cs140e-25win//libpi/include/gpio.h \
+ /home/engler/class/cs140e-25win//libpi/include/rpi-constants.h \
+ /home/engler/class/cs140e-25win//libpi/include/your-prototypes.h \
+ external-code/unicode-utf8.h
+
+/home/engler/class/cs140e-25win//libpi/include/rpi.h:
+
+/home/engler/class/cs140e-25win//libpi/libc/demand.h:
+
+/home/engler/class/cs140e-25win//libpi/include/gpio.h:
+
+/home/engler/class/cs140e-25win//libpi/include/rpi-constants.h:
+
+/home/engler/class/cs140e-25win//libpi/include/your-prototypes.h:
+
+external-code/unicode-utf8.h:
---
+++ labs/16-gpu/code/2-mandelbrot/fat32/code/objs/mbr-helpers.d
@@ -0,0 +1,23 @@
+objs/mbr-helpers.o: mbr-helpers.c \
+ /home/engler/class/cs140e-25win//libpi/include/rpi.h \
+ /home/engler/class/cs140e-25win//libpi/libc/demand.h \
+ /home/engler/class/cs140e-25win//libpi/include/gpio.h \
+ /home/engler/class/cs140e-25win//libpi/include/rpi-constants.h \
+ /home/engler/class/cs140e-25win//libpi/include/your-prototypes.h mbr.h \
+ pi-sd.h mbr-helpers.h
+
+/home/engler/class/cs140e-25win//libpi/include/rpi.h:
+
+/home/engler/class/cs140e-25win//libpi/libc/demand.h:
+
+/home/engler/class/cs140e-25win//libpi/include/gpio.h:
+
+/home/engler/class/cs140e-25win//libpi/include/rpi-constants.h:
+
+/home/engler/class/cs140e-25win//libpi/include/your-prototypes.h:
+
+mbr.h:
+
+pi-sd.h:
+
+mbr-helpers.h:
